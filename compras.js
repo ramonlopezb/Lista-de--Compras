b10051b48c207476ea26b89bbd5025316c3436be
ramonlopezb <ramonlb54@gmail.com>
2023-06-01 13:13:31 -0300

    creacion de lista de compras vegetales
    Elabore los div , selector, listas

--- compras.js
+++ compras.js
@@ -8,21 +8,81 @@ parrafo.innerHTML = "comienza la funcion con js";//insertamos texto al parrafo//
 
 //vamos a insertar el parrafo en el div, se puede insertar cualkquier elemento//
 //contenedorDiv.appendChild(parrafo);decimos que parrafo es hijo de contenedor div//
-const legumbres = document.getElementById("container");
+const section = document.getElementById("section");
 const vegetales = ["Tomates","Cebollas","Brocolis", "Ajo", "aji"];
+const carniceria = document.getElementById("container1");
+const carnes = ["Asado","Nalga","Peseto", "Picada", "Vacio"];
+const hogar = document.getElementById("container2");
+const lenceria = ["Vasos","Platos","Sabanas", "Copas", "Cepillos"];
+const btnincorporarProducto = document.querySelector(".boton");
+let selectorVegetales = document.getElementById("selector-vegetales");
+let selectorCarnes = document.getElementById("selector-carnes");
+let selectorHogar = document.getElementById("selector-hogar");
+let btnVegetales = document.getElementById("btn-vegetales");
+let btnCarniceria = document.getElementById("btn-carniceria");
+let btnHogar = document.getElementById("btn-hogar");
+
+let listaCompras = [];
+let seleccionar ;
+let vegetalSeleccionado;
+let carneSeleccionada;
+let hogarSeleccionado;
+
+section.innerHTML = "";
+
+selectorVegetales.addEventListener('change', function () {
+        vegetalSeleccionado = (this.value);
+        });
+//selectorCarnes.addEventListener('change', function () {//
+ //       carneSeleccionada = (this.value);//
+            //    });//
+//selectorHogar.addEventListener('change', function () {//
+       // hogarSeleccionado = (this.value);//
+           //             });//
+
+
+function seleccionVegetales(){
+        if(!selectorVegetales == 0){
+
+        let padre = document.createElement("div");
+        section.appendChild(padre);
+        padre.classList.add("item"); 
+
+        let prefijo = document.createElement("div");
+        prefijo.id = "letraA";
+        section.appendChild(prefijo);
+        prefijo.innerHTML =` <input type = "checkbox" >` ; 
+
+        let nombreDelItem = document.createElement("div");
+        nombreDelItem.id = "tomate";
+        section.appendChild(nombreDelItem);
+        nombreDelItem.innerHTML = `${vegetalSeleccionado}`; 
+
+        let sufijo = document.createElement("div");
+        sufijo.id = "letraB";
+        section.appendChild(sufijo);
+        sufijo.innerHTML = `-` ;  
+
+
+        padre.appendChild(prefijo);
+        padre.appendChild(nombreDelItem);
+        padre.appendChild(sufijo);}
+
+}
+seleccionVegetales();
+
+btnVegetales.addEventListener("click", seleccionVegetales);
+
+
    
 
 
-legumbres.innerHTML = "";
-function seleccionarVegetales(){
-        for(i = 0, i < vegetales.length; i++;){
-                const element = vegetales[i];
-                let div = document.createElement("div");
-                legumbres.appendChild(div);
-                div.innerHTML = vegetales[i] ;  
-                }
-                
-        };         
-               
 
-seleccionarVegetales();
+
+
+        
+                            
+            
+        
+
+            
